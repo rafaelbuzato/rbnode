b80b7249a83f7667c022f5af00f43c6064dc3017
rafaelbuzato <39388205+rafaelbuzato@users.noreply.github.com>
2018-06-01 09:10:51 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à RBPendrives!'
 module.exports ={
   connectionString:'senha',
   sendgridKey: 'Senha',
-  containerConnectionString:'DefaultEndpointsProtocol=https;AccountName=rbpendrives;AccountKey=aSeuxcR7sXq3K4gXMoDQWWhbq8vshgChzgORKDaH0k1RiPZvkDoYhGhEc/e/Ttiz01ACFDp/I5ih4ySJj99kyw==;EndpointSuffix=core.windows.net'
+  containerConnectionString:'conexão'
 }
