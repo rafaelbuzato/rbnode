6b51f8456c7981ced2a62bde9c5fa028ed863bc0
rafaelbuzato <39388205+rafaelbuzato@users.noreply.github.com>
2018-06-01 09:01:39 -0300

    Update and rename src/config.js to  src/config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à RBPendrives!'
 
 module.exports ={
   connectionString:'senha',
-  //https://app.sendgrid.com/settings/api_keys
   sendgridKey: 'Senha',
   containerConnectionString:'DefaultEndpointsProtocol=https;AccountName=rbpendrives;AccountKey=aSeuxcR7sXq3K4gXMoDQWWhbq8vshgChzgORKDaH0k1RiPZvkDoYhGhEc/e/Ttiz01ACFDp/I5ih4ySJj99kyw==;EndpointSuffix=core.windows.net'
 }
