58f099f0b99d214dfe4763423d944138a5651e01
rafaelbuzato <39388205+rafaelbuzato@users.noreply.github.com>
2018-06-01 08:49:52 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = 'chave';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à RBPendrives!';
 
 module.exports ={
-  connectionString:'mongodb://user:user123@ds157980.mlab.com:57980/bdteste',
+  connectionString:'senha',
   //https://app.sendgrid.com/settings/api_keys
-  sendgridKey: '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',
+  sendgridKey: 'Senha',
   containerConnectionString:'DefaultEndpointsProtocol=https;AccountName=rbpendrives;AccountKey=aSeuxcR7sXq3K4gXMoDQWWhbq8vshgChzgORKDaH0k1RiPZvkDoYhGhEc/e/Ttiz01ACFDp/I5ih4ySJj99kyw==;EndpointSuffix=core.windows.net'
 }
